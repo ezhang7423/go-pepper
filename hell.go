e8fea64f33bb7870d936f7680bce5a3d8eaf5c2a
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2019-12-22 21:40:34 -0800

    ok

--- hell.go
+++ hell.go
@@ -3,16 +3,21 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 )
 
 var x, y = 2, 4
 var x1, y1 = float64(x), float64(y)
 
+func print(thing string) {
+	fmt.Println(thing)
+}
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
 	for i := 0; i < 10; i++ {
 		if i%2 == 0 {
-			fmt.Println(i)
+			tmp := strconv.Itoa(i)
+			print(tmp)
 		}
 	}
 }
